f1ec6a137f72846268a7aa2b7c6565741dfaeb47
tamonmon0417 <tamonmon0417@gmail.com>
2022-10-09 12:24:12 +0900

    fin

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,10 +1,18 @@
 import { useState } from 'react'
+import { useActions } from '../hooks/useActions'
+import { useTypedSelector } from '../hooks/useTypedSelector'
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('')
+  const { searchRepositories } = useActions()
+  const { data, error, loading } = useTypedSelector(
+    (state) => state.repositories
+  )
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
+
+    searchRepositories(term)
   }
 
   return (
@@ -17,6 +25,9 @@ const RepositoriesList: React.FC = () => {
         />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data}
     </div>
   )
 }
